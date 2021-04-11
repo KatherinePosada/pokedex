febadb0728715f4e97b1b8148d46238a6365df85
Katherine Posada <kathepoar@gmail.com>
2021-04-10 21:48:17 -0500

    :sparkles: New feature :: Welcome view

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from "bootstrap-vue";
-import SuiVue from 'semantic-ui-vue';
+import BootstrapVueOK from "bootstrap-vue";
+//import SuiVue from 'semantic-ui-vue';
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue);
-Vue.use(SuiVue);
+Vue.use(BootstrapVueOK);
+//Vue.use(SuiVue);
 
 new Vue({
   render: h => h(App),
